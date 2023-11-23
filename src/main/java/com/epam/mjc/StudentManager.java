2d75e6cea94ab77747b0ad780b7afe5bb10ac60b
Muzaffar55 <97522350+Muzaffar55@users.noreply.github.com>
2023-11-23 20:49:11 +0500

    Update StudentManager.java
    Done first attempt

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,21 +1,35 @@
 package com.epam.mjc;
 
-
 public class StudentManager {
 
-  private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
-  }
+    public class StudentNotFoundException extends RuntimeException {
 
-  public static void main(String[] args) {
-    StudentManager manager = new StudentManager();
-
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+        public StudentNotFoundException(String message, Throwable cause) {
+            super(message, cause);
+        }
     }
 
-  }
+    public Student find(long studentID) {
+        try {
+            return Student.getValueOf(studentID);
+        } catch (IllegalArgumentException e) {
+            // Talaba topilmaganida istisno chiqarish
+            throw new StudentNotFoundException("Student not found with ID: " + studentID, e);
+        }
+    }
+
+    public static void main(String[] args) {
+        StudentManager manager = new StudentManager();
+
+        for (int i = 0; i < IDs.length; i++) {
+            try {
+                Student student = manager.find(IDs[i]);
+                System.out.println("Student name: " + student.getName());
+            } catch (StudentNotFoundException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
